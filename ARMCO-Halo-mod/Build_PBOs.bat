b50d3430c3ef4d840f92b2e0a639c6511eff6400
Belhun <akcool151@gmail.com>
2023-03-16 08:01:48 -0700

    updated bat file

--- ARMCO-Halo-mod/Build_PBOs.bat
+++ ARMCO-Halo-mod/Build_PBOs.bat
@@ -2,10 +2,10 @@
 C:
 cd C:\Program Files\PBO Manager v.1.4 beta
 
-PBOConsole.exe -pack P:\Armco-Halo-Mod\ARMCO-Halo-mod\custom_armours P:\Exported\@Belhun_Testing\Addons\custom_armours.pbo
-PBOConsole.exe -pack P:\Armco-Halo-Mod\ARMCO-Halo-mod\custom_dress_uniforms P:\Exported\@Belhun_Testing\Addons\custom_dress_uniforms.pbo
-PBOConsole.exe -pack P:\Armco-Halo-Mod\ARMCO-Halo-mod\custom_emergency P:\Exported\@Belhun_Testing\Addons\custom_emergency.pbo
-PBOConsole.exe -pack P:\Armco-Halo-Mod\ARMCO-Halo-mod\custom_groups P:\Exported\@Belhun_Testing\Addons\custom_groups.pbo
-PBOConsole.exe -pack P:\Armco-Halo-Mod\ARMCO-Halo-mod\custom_vehicles P:\Exported\@Belhun_Testing\Addons\custom_vehicles.pbo
+PBOConsole.exe -pack E:\Code\ARMA3\Armco-Halo-Mod\ARMCO-Halo-mod\custom_armours        E:\Code\ARMA3\Export\@armcohalo\Addons\custom_armours.pbo
+PBOConsole.exe -pack E:\Code\ARMA3\Armco-Halo-Mod\ARMCO-Halo-mod\custom_dress_uniforms E:\Code\ARMA3\Export\@armcohalo\Addons\custom_dress_uniforms.pbo
+PBOConsole.exe -pack E:\Code\ARMA3\Armco-Halo-Mod\ARMCO-Halo-mod\custom_emergency      E:\Code\ARMA3\Export\@armcohalo\Addons\custom_emergency.pbo
+PBOConsole.exe -pack E:\Code\ARMA3\Armco-Halo-Mod\ARMCO-Halo-mod\custom_groups         E:\Code\ARMA3\Export\@armcohalo\Addons\custom_groups.pbo
+PBOConsole.exe -pack E:\Code\ARMA3\Armco-Halo-Mod\ARMCO-Halo-mod\custom_vehicles       E:\Code\ARMA3\Export\@armcohalo\Addons\custom_vehicles.pbo
 
 PAUSE
