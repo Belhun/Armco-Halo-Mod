f176bd0bd895dc59436ac416a702192ddb0866c2
Belhun <akcool151@gmail.com>
2023-04-03 22:47:04 -0700

    changed location path to have an _ in it

--- example/@armcohalo/mod.cpp
+++ example/@armcohalo/mod.cpp
@@ -1,4 +1,21 @@
-name = "Armco Halo [v0.1.0]";
+name		 = "Armco Halo [v0.1.0]";								// Name of your mod
+picture 	 = "armco_armours\data\armco_imgs\armco4thbat2048.paa"; 	// Picture displayed from the expansions menu. Optimal size is 2048x1024
+logoSmall	 = "armco_armours\data\armco_imgs\armco4thbat64.paa";	// Display next to the item added by the mod
+logo		 = "armco_armours\data\armco_imgs\armco4thbat128.paa";			// Logo displayed in the main menu
+logoOver	 = "armco_armours\data\armco_imgs\armco4thbat512.paa";		// When the mouse is over, in the main menu
+action		 = "https://github.com/Belhun/Armco-Halo-Mod";			// Website URL, that can accessed from the expansions menu
+tooltipOwned = "Armco Halo";								// Tool tip displayed when the mouse is left over, in the main menu
 author = "Belhun/Armco";
-actionName = "Website";
-action = "https://github.com/Belhun/Armco-Halo-Mod";
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
+// Overview text, displayed from the extension menu
+overview = "Unleash your creativity! Here goes the long description of your mod.";
+hideName	= 0;	// Hide the extension name
+hidePicture	= 0;	// Hide the extension menu
