81f3992f1c148df73bbfbf761da66ebd72353f08
Belhun <akcool151@gmail.com>
2023-04-05 00:08:22 -0700

    Changed names and added a new .hpp file for Orginization

--- ARMCO-Halo-mod/armco_armours/config.cpp
+++ ARMCO-Halo-mod/armco_armours/config.cpp
@@ -6,11 +6,11 @@ class CfgVehicles{
 	
 
 };
-
+// cfgWeapons includes weapons armours and helmets and uniforms.
 class cfgWeapons {
 	//this include brings in all the all the Helmets from the CfgVehicleHelmet.hpp
 	//btw this include when compiled is palced here on this line
-	#include "CfgWeaponsHelmet.hpp"
+	#include "CfgWeaponsSHelmet.hpp"
 	
 	//this include brings in all the all the Uniforms from the CfgVehicleUniform.hpp
 	//btw this include when compiled is palced here on this line
@@ -19,4 +19,8 @@ class cfgWeapons {
 	//this include brings in all the all the Vests from the CfgVehicleVest.hpp
 	//btw this include when compiled is palced here on this line
 	#include "CfgWeaponsSArmour.hpp"
+
+
+	//this include brings in all the all the Custom armour from the CfgWeaponsCArmour.hpp
+	#include "CfgWeaponsCArmour.hpp"
 };
