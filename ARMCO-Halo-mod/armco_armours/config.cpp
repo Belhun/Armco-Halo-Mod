e990c21a6da0712cbfadc65a4e55e915bea101e7
Belhun <akcool151@gmail.com>
2023-04-05 00:10:54 -0700

    added another file for custom helmets

--- ARMCO-Halo-mod/armco_armours/config.cpp
+++ ARMCO-Halo-mod/armco_armours/config.cpp
@@ -3,21 +3,19 @@
 
 class CfgVehicles{
 	#include "CfgVehicleSoldier.hpp"
-	
-
 };
 // cfgWeapons includes weapons armours and helmets and uniforms.
 class cfgWeapons {
 	//this include brings in all the all the Helmets from the CfgVehicleHelmet.hpp
-	//btw this include when compiled is palced here on this line
 	#include "CfgWeaponsSHelmet.hpp"
+
+	//Brings in all the custom helmets from the CfgWeaponsCHelmet.hpp
+	#include "CfgWeaponsCHelmet.hpp"
 	
 	//this include brings in all the all the Uniforms from the CfgVehicleUniform.hpp
-	//btw this include when compiled is palced here on this line
 	#include "CfgWeaponsUniform.hpp"
 	
 	//this include brings in all the all the Vests from the CfgVehicleVest.hpp
-	//btw this include when compiled is palced here on this line
 	#include "CfgWeaponsSArmour.hpp"
 
 
