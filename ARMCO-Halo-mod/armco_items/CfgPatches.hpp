a3149dbf11e82516971b3976068c17632006e861
Belhun <akcool151@gmail.com>
2023-04-09 21:56:01 -0700

    Changed CfgPatches for backpacks under armco_items

--- ARMCO-Halo-mod/armco_items/CfgPatches.hpp
+++ ARMCO-Halo-mod/armco_items/CfgPatches.hpp
@@ -4,15 +4,16 @@ class CfgPatches
 	class ARMCO_Units_Army { 
 		units[]=
 		{
-			"ARMCO_ILCS_Rucksack_Heavy_Alpha",
-			"ARMCO_ILCS_Rucksack_Heavy_Sabre",
-			"ARMCO_ILCS_Rucksack_Heavy_Zulu",
-			"ARMCO_ILCS_Rucksack_Heavy_Phoenix",
-			"ARMCO_ILCS_Rucksack_Heavy_Medical",
-			"ARMCO_Rucksack_Basic",
-			"ARMCO_Rucksack_B_Addons",
-			"ARMCO_Rucksack_B_Medic",
-			"ARMCO_Rucksack_B_Radio"
+			"ARMCO_Rucksack_Hard_Alpha",
+			"ARMCO_Rucksack_Hard_Sabre",
+			"ARMCO_Rucksack_Hard_Zulu",
+			"ARMCO_Rucksack_Hard_Phoenix",
+			"ARMCO_Rucksack_Hard_Medical",
+			"ARMCO_Rucksack_Soft_Alpha",
+			"ARMCO_Rucksack_Soft_Sabre",
+			"ARMCO_Rucksack_Soft_Zulu",
+			"ARMCO_Rucksack_Soft_Phoenix",
+			"ARMCO_Rucksack_Soft_Medical"
 		};
 		weapons[]={};
 		requiredVersion = 0.1; 
