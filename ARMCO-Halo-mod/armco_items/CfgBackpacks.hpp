0e4e117c58d5654f4844b23c0ddc6d867c324c5f
Belhun <akcool151@gmail.com>
2023-04-09 21:31:03 -0700

    change file name

---
+++ ARMCO-Halo-mod/armco_items/CfgBackpacks.hpp
@@ -0,0 +1,166 @@
+class OPTRE_ILCS_Rucksack_Black;
+	//ODST Backpacks
+	class ARMCO_ILCS_Rucksack_Heavy_Alpha: OPTRE_ILCS_Rucksack_Black
+	{
+		dlc="OPTRE";
+		author="Article 2 Studios";
+		maximumLoad=400;
+		mass=50;
+		displayName="[A][ARMCO]Hard Rucksack [Alpha]";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"biofoam"
+		};
+        hiddenSelectionsTextures[]=
+		{
+			"armco_items\data\backpacks\backpack_odst_hard_alpha_co.paa"
+		};
+	};
+	class ARMCO_ILCS_Rucksack_Heavy_Sabre: OPTRE_ILCS_Rucksack_Black
+	{
+		dlc="OPTRE";
+		author="Article 2 Studios";
+		maximumLoad=400;
+		mass=50;
+		displayName="[A][ARMCO]Hard Rucksack [Sabre]";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"biofoam"
+		};
+        hiddenSelectionsTextures[]=
+		{
+			"armco_items\data\backpacks\backpack_odst_hard_sabre_co.paa"
+		};
+	};
+	class ARMCO_ILCS_Rucksack_Heavy_Zulu: OPTRE_ILCS_Rucksack_Black
+	{
+		dlc="OPTRE";
+		author="Article 2 Studios";
+		maximumLoad=400;
+		mass=50;
+		displayName="[A][ARMCO]Hard Rucksack [Zulu]";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"biofoam"
+		};
+        hiddenSelectionsTextures[]=
+		{
+			"armco_items\data\backpacks\backpack_odst_hard_zulu_co.paa"
+		};
+	};
+	class ARMCO_ILCS_Rucksack_Heavy_Phoenix: OPTRE_ILCS_Rucksack_Black
+	{
+		dlc="OPTRE";
+		author="Article 2 Studios";
+		maximumLoad=400;
+		mass=50;
+		displayName="[A][ARMCO]Hard Rucksack [Phoenix]";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"biofoam"
+		};
+        hiddenSelectionsTextures[]=
+		{
+			"armco_items\data\backpacks\backpack_odst_hard_phoenix_co.paa"
+		};
+	};
+	class ARMCO_ILCS_Rucksack_Heavy_Medical: OPTRE_ILCS_Rucksack_Black
+	{
+		dlc="OPTRE";
+		author="Article 2 Studios";
+		maximumLoad=400;
+		mass=50;
+		displayName="[A][ARMCO]Hard Rucksack [Medical]";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"ap_heavy"
+		};
+        hiddenSelectionsTextures[]=
+		{
+			"armco_items\data\backpacks\backpack_odst_hard_medical_co.paa"
+		};
+	};
+
+
+	
+	//Basic Backpacks
+	class OPTRE_UNSC_Rucksack;
+	class ARMCO_Rucksack_Basic: OPTRE_UNSC_Rucksack
+	{
+		dlc="OPTRE";
+		author="Article 2 Studios";
+		displayName="[A][ARMCO] Rucksack (Basic)";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2",
+			"B_Addons",
+			"B_Medic",
+			"B_Radio",
+			"B_strap"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"optre_unsc_units\army\data\soft_backpack_co.paa",
+			""
+		};
+	};
+	class ARMCO_Rucksack_B_Addons: OPTRE_UNSC_Rucksack
+	{
+		dlc="OPTRE";
+		author="Article 2 Studios";
+		displayName="[A][ARMCO] Rucksack (B_Addons)";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2",
+			"B_Medic",
+			"B_Radio"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"optre_unsc_units\army\data\soft_backpack_co.paa",
+			""
+		};
+	};
+	class ARMCO_Rucksack_B_Medic: OPTRE_UNSC_Rucksack
+	{
+		dlc="OPTRE";
+		author="Article 2 Studios";
+		displayName="[A][ARMCO] Rucksack (B_Medic)";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2",
+			"B_Addons",
+			"B_Radio"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"optre_unsc_units\army\data\soft_backpack_co.paa",
+			""
+		};
+	};
+	class ARMCO_Rucksack_B_Radio: OPTRE_UNSC_Rucksack
+	{
+		dlc="OPTRE";
+		author="Article 2 Studios";
+		displayName="[A][ARMCO] Rucksack (B_Radio)";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2",
+			"B_Addons",
+			"B_Medic",
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"optre_unsc_units\army\data\soft_backpack_co.paa",
+			""
+		};
+	};
---
+++ ARMCO-Halo-mod/armco_items/CfgPatches.hpp
@@ -0,0 +1,21 @@
+//this CfgPatches is for Backpacks
+class CfgPatches 
+{
+	class ARMCO_Units_Army { 
+		units[]=
+		{
+			"ARMCO_ILCS_Rucksack_Heavy_Alpha",
+			"ARMCO_ILCS_Rucksack_Heavy_Sabre",
+			"ARMCO_ILCS_Rucksack_Heavy_Zulu",
+			"ARMCO_ILCS_Rucksack_Heavy_Phoenix",
+			"ARMCO_ILCS_Rucksack_Heavy_Medical",
+			"ARMCO_Rucksack_Basic",
+			"ARMCO_Rucksack_B_Addons",
+			"ARMCO_Rucksack_B_Medic",
+			"ARMCO_Rucksack_B_Radio"
+		};
+		weapons[]={};
+		requiredVersion = 0.1; 
+		requiredAddons[] = {"OPTRE_UNSC_Units","A3_Weapons_F","OPTRE_Core" ,"ace_main","A3_Characters_F_BLUFOR","OPTRE_Weapons"}; 
+	}; 
+};
