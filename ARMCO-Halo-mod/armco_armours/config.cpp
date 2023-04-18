2d0fd7d1001ef441765f80a09a7a88f09b8d1761
Belhun <akcool151@gmail.com>
2023-04-17 19:40:35 -0700

    Moved includes around

--- ARMCO-Halo-mod/armco_armours/config.cpp
+++ ARMCO-Halo-mod/armco_armours/config.cpp
@@ -10,17 +10,17 @@ class cfgWeapons {
 // this include brings in all the all the Helmets from the CfgVehicleHelmet.hpp
 #include "CfgWeaponsSHelmet.hpp"
 
-// Brings in all the custom helmets from the CfgWeaponsCHelmet.hpp
-#include "CfgWeaponsCHelmet.hpp"
-
 // this include brings in all the all the Uniforms from the
 // CfgVehicleUniform.hpp
 #include "CfgWeaponsUniform.hpp"
 
-// this include brings in all the all the Vests from the CfgVehicleVest.hpp
+// this include Squad based Armours from the CfgWeaponsSArmour.hpp
 #include "CfgWeaponsSArmour.hpp"
 
 // this include brings in all the all the Custom armour from the
 // CfgWeaponsCArmour.hpp
 #include "CfgWeaponsCArmour.hpp"
+
+// Brings in all the custom helmets from the CfgWeaponsCHelmet.hpp
+#include "CfgWeaponsCHelmet.hpp"
 };
