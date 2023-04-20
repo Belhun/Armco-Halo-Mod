768ac318c52ad18705fd148a99aa4498e404d058
Belhun <akcool151@gmail.com>
2023-04-20 00:28:35 -0700

    Updated Custom Helmets: Fixed bug
     so when the hud is turned on and off the helemet dosent disapeir. also got rid of the Item info section as i was told and proven that its un nessery for it too be there.
    Darkness helped me understand it all and i changed most of it.
    STILL NEED TO PUT CLEAR TEXTURE ON ALL THE _dp Helmets in CfgWeponsCHelmet.hpp
    
    Co-Authored-By: Darknessvoid99 <63525965+Darknessvoid99@users.noreply.github.com>

--- ARMCO-Halo-mod/armco_armours/CfgWeaponsCHelmet.hpp
+++ ARMCO-Halo-mod/armco_armours/CfgWeaponsCHelmet.hpp
@@ -17,21 +17,12 @@ class Addy_CH252D_Helmet : Alpha_CH252D_Helmet {
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Addy_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Addy_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Addy_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Addy_co.paa"};
-  };
 };
 class Belhun_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Belhun)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Belhun_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Belhun_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Belhun_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Belhun_co.paa"};
   };
 };
 class Blue_CH252D_Helmet : Alpha_CH252D_Helmet {
@@ -39,208 +30,325 @@ class Blue_CH252D_Helmet : Alpha_CH252D_Helmet {
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Blue_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Blue_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Blue_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Blue_co.paa"};
-  };
 };
 class Burford_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Burford)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Burford_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Burford_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Burford_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Burford_co.paa"};
-  };
 };
 class Boone_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Boone)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Boone_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Boone_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Boone_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Boone_co.paa"};
-  };
 };
 class Cheese_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Cheese)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Cheese_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_ciro_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Cheese_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_ciro_co.paa"};
-  };
 };
 class Ciro_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Ciro)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Ciro_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_ciro_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Ciro_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_ciro_co.paa"};
-  };
 };
 class Darkness_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Darkness)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Darkness_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Darkness_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Darkness_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Darkness_co.paa"};
-  };
 };
 class Darman_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Darman)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Darman_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Darman_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Darman_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Darman_co.paa"};
-  };
 };
 class Extortionist_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Extortionist)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Extortionist_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Extortionist_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Extortionist_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Extortionist_co.paa"};
-  };
 };
 class Garfield_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Garfield)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Garf_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Garf_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Garf_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Garf_co.paa"};
-  };
 };
 class Merek_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Merek)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Merek_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Merek_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Merek_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Merek_co.paa"};
-  };
 };
 class Mini_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Mini)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Mini_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Mini_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Mini_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Mini_co.paa"};
-  };
 };
 class Nutters_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Nutters)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Nutters_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Nutters_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Nutters_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Nutters_co.paa"};
-  };
 };
 class Raziel_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Raziel)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Raz_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_raz_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Raz_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_raz_co.paa"};
-  };
 };
 class Reaper_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Reaper)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Reaper_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Reaper_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Reaper_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Reaper_co.paa"};
-  };
 };
 class Red_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Red)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_RedDragon_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_RedDragon_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_RedDragon_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_RedDragon_co.paa"};
-  };
 };
 class Runic_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Runic)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Runic_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Runic_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Runic_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Runic_co.paa"};
-  };
 };
 class Shaw_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Shaw)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Shaw_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Shaw_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Shaw_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Shaw_co.paa"};
-  };
 };
 class VJ_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (VJ)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_VJ_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_VJ_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_VJ_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_VJ_co.paa"};
-  };
 };
 class Wolffe_CH252D_Helmet : Alpha_CH252D_Helmet {
   displayName = "[A][ARMCO] ODST Helmet (Wolffe)";
   hiddenSelectionsTextures[] = {
       "armco_armours\data\odst\custom_helmets\odst_helmet_Wolffe_co.paa",
       "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Wolffe_co.paa"};
-  class ItemInfo : ItemInfo {
-    hiddenSelectionsTextures[] = {
-        "armco_armours\data\odst\custom_helmets\odst_helmet_Wolffe_co.paa",
-        "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Wolffe_co.paa"};
-  };
 };
+
+//DP VERSIONS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Addy_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Addy)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Addy_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Addy_co.paa"};
+};
+class Belhun_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Belhun)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Belhun_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Belhun_co.paa"};
+
+};
+class Blue_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Blue)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Blue_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Blue_co.paa"};
+};
+class Burford_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Burford)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Burford_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Burford_co.paa"};
+};
+class Boone_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Boone)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Boone_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Boone_co.paa"};
+};
+class Cheese_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Cheese)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Cheese_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_ciro_co.paa"};
+};
+class Ciro_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Ciro)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Ciro_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_ciro_co.paa"};
+};
+class Darkness_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Darkness)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Darkness_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Darkness_co.paa"};
+};
+class Darman_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Darman)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Darman_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Darman_co.paa"};
+};
+class Extortionist_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Extortionist)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Extortionist_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Extortionist_co.paa"};
+};
+class Garfield_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Garfield)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Garf_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Garf_co.paa"};
+};
+class Merek_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Merek)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Merek_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Merek_co.paa"};
+};
+class Mini_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Mini)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Mini_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Mini_co.paa"};
+};
+class Nutters_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Nutters)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Nutters_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Nutters_co.paa"};
+};
+class Raziel_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Raziel)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Raz_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_raz_co.paa"};
+};
+class Reaper_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Reaper)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Reaper_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Reaper_co.paa"};
+};
+class Red_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Red)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_RedDragon_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_RedDragon_co.paa"};
+};
+class Runic_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Runic)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Runic_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Runic_co.paa"};
+};
+class Shaw_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Shaw)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Shaw_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Shaw_co.paa"};
+};
+class VJ_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (VJ)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_VJ_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_VJ_co.paa"};
+};
+class Wolffe_CH252D_Helmet_dp : Alpha_CH252D_Helmet {
+  scope = 1;
+  scopeCurator = 1;
+  ScopeArsenal = 1;
+  displayName = "[A][ARMCO] ODST Helmet (Wolffe)";
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\odst\custom_helmets\odst_helmet_Wolffe_co.paa",
+      "armco_armours\data\odst\custom_helmets\visors\odst_helmet_visor_Wolffe_co.paa"};
+};
+
+
 
