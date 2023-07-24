6867334e1e02c09e377d61f09b26c5de57556684
Belhun <akcool151@gmail.com>
2023-07-24 02:48:12 -0700

    Removed comments

--- ARMCO-Halo-mod/armco_armours/CfgArsonalExspa.hpp
+++ ARMCO-Halo-mod/armco_armours/CfgArsonalExspa.hpp
@@ -1,70 +1,7 @@
-
-        // Alpha_CH252_Helmet
-        // Sabre_CH252_Helmet
-        // Phoenix_CH252_Helmet
-        // Zulu_CH252_Helmet
-        // Medical_CH252_Helmet
-        // EOD_CH252_Helmet
-        // ARMCO_Generic_CH252_Ghillie_Helmet
-
-
-        // Alpha_CH252D_Helmet
-        // Sabre_CH252D_Helmet
-        // Zulu_CH252D_Helmet
-        // Phoenix_CH252D_Helmet
-        // ARMCO_PMC_Army_Uniform_WDL
-
-
-        // Alpha_BDU
-        // Alpha_EOD_BDU
-        // Alpha_Medic_BDU
-        // Sabre_BDU
-        // Sabre_EOD_BDU
-        // Sabre_Medic_BDU
-        // Zulu_BDU
-        // Zulu_Camo_BDU
-        // Zulu_EOD_BDU
-        // Zulu_Medic_BDU
-        // Zulu_Medic_Camo_BDU
-        // Phoenix_BDU
-        // Phoenix_EOD_BDU
-        // Phoenix_Medic_BDU
-
-
-        // ARMCO_ODST_BLK_BDU
-        // ARMCO_ODST_camo_BDU
-        // ARMCO_ODST_M90_BDU
-        // ARMCO_ODST_GDJungle_BDU
-        // ARMCO_ODST_GDDMOUNT_BDU
-        // ARMCO_ODST_DDRKWDL_BDU
-        // ARMCO_ODST_DWLD_BDU
-        // ARMCO_SEC_OVAL_BDU
-        // ARMCO_ODST_SEC_BDU
-
-
-        // ARMCO_Alpha_Armor
-        // ARMCO_Sabre_Armor
-        // ARMCO_Zulu_Armor
-        // ARMCO_Phoenix_Armor
-        // ARMCO_Medical_Armor
-        // ARMCO_EOD_Armor
-        // ARMCO_Zulu_Ghillie_Armor
-
-        // ARMCO_Alpha_ODST_Armor
-        // ARMCO_Sabre_ODST_Armor
-        // ARMCO_Zulu_ODST_Armor
-        // ARMCO_Phoenix_ODST_Armor
-
 class XtdGearInfos
 {
 	class CfgWeapons
 	{
-        class ARMCO_Base_Armor
-        {
-            Model="ARMCO_Base_Armor";
-            Squads="Base";
-            Addons="No_Ghillie";
-        };
         class ARMCO_Alpha_Armor
         {
             Model="ARMCO_Base_Armor";
