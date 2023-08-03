da00e2ffe8b324b88d446249b746642dea973e67
Belhun <akcool151@gmail.com>
2023-08-03 00:00:42 -0700

    added Alpha Vereiant armors

---
+++ ARMCO-Halo-mod/armco_armours/CfgWeaponsSArmourVariants.hpp
@@ -0,0 +1,265 @@
+class ARMCO_Alpha_Team_1_Armor : ARMCO_Alpha_Armor {
+  dlc = "ARMCO";
+  scope = 2;
+  ScopeArsenal = 2;
+  author = "Belhun";
+  displayName = "[ARMCO][A] Alpha Armor Team 1";
+  class ItemInfo: ItemInfo
+  {
+    hiddenSelections[] = {"camo",
+                        "camo2",
+                        "camo3",
+                        "camo4",
+                        "camo5",
+                        "A_Ghillie",
+                        "A_KneesLeft",
+                        "A_KneesRight",
+                        "A_ODST",
+                        "A_TacPad",
+                        "AS_BaseLeft",
+                        "AS_BaseRight",
+                        "AS_LargeRight",
+                        "AS_MediumLeft",
+                        "AS_ODSTCQBLeft",
+                        "AS_ODSTCQBRight",
+                        "AS_ODSTLeft",
+                        "AS_ODSTRight",
+                        "AS_ODSTSniperLeft",
+                        "AS_ODSTSniperRight",
+                        "AS_SmallLeft",
+                        "AS_SmallRight",
+                        "AP_BR",
+                        "AP_Canteen",
+                        "AP_GL",
+                        "AP_Knife",
+                        "AP_MGThigh",
+                        "AP_Rounds",
+                        "AP_SG",
+                        "AP_SMG",
+                        "AP_Sniper",
+                        "AP_Frag",
+                        "APO_AR",
+                        "APO_BR",
+                        "APO_SMG",
+                        "APO_Sniper",
+                        "CustomKit_Scorch"};
+  }
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\marine\armors\vest_alpha_co.paa",
+      "armco_armours\data\marine\armors\armour_alpha_1_co.paa",
+      "armco_armours\data\marine\armors\legs_alpha_co.paa",
+      "optre_unsc_units\army\data\ghillie_woodland_co.paa",
+      "optre_unsc_units\army\data\odst_armor_co.paa"};
+};
+class ARMCO_Alpha_Team_2_Armor : ARMCO_Alpha_Armor {
+  dlc = "ARMCO";
+  scope = 2;
+  ScopeArsenal = 2;
+  author = "Belhun";
+  displayName = "[ARMCO][A] Alpha Armor Team 2";
+  class ItemInfo: ItemInfo
+  {
+    hiddenSelections[] = {"camo",
+                        "camo2",
+                        "camo3",
+                        "camo4",
+                        "camo5",
+                        "A_Ghillie",
+                        "A_KneesLeft",
+                        "A_KneesRight",
+                        "A_ODST",
+                        "A_TacPad",
+                        "AS_BaseLeft",
+                        "AS_BaseRight",
+                        "AS_LargeRight",
+                        "AS_MediumLeft",
+                        "AS_ODSTCQBLeft",
+                        "AS_ODSTCQBRight",
+                        "AS_ODSTLeft",
+                        "AS_ODSTRight",
+                        "AS_ODSTSniperLeft",
+                        "AS_ODSTSniperRight",
+                        "AS_SmallLeft",
+                        "AS_SmallRight",
+                        "AP_BR",
+                        "AP_Canteen",
+                        "AP_GL",
+                        "AP_Knife",
+                        "AP_MGThigh",
+                        "AP_Rounds",
+                        "AP_SG",
+                        "AP_SMG",
+                        "AP_Sniper",
+                        "AP_Frag",
+                        "APO_AR",
+                        "APO_BR",
+                        "APO_SMG",
+                        "APO_Sniper",
+                        "CustomKit_Scorch"};
+  }
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\marine\armors\vest_alpha_co.paa",
+      "armco_armours\data\marine\armors\armour_alpha_2_co.paa",
+      "armco_armours\data\marine\armors\legs_alpha_co.paa",
+      "optre_unsc_units\army\data\ghillie_woodland_co.paa",
+      "optre_unsc_units\army\data\odst_armor_co.paa"};
+};
+class ARMCO_Alpha_Team_3_Armor : ARMCO_Alpha_Armor {
+  dlc = "ARMCO";
+  scope = 2;
+  ScopeArsenal = 2;
+  author = "Belhun";
+  displayName = "[ARMCO][A] Alpha Armor Team 3";
+  class ItemInfo: ItemInfo
+  {
+    hiddenSelections[] = {"camo",
+                        "camo2",
+                        "camo3",
+                        "camo4",
+                        "camo5",
+                        "A_Ghillie",
+                        "A_KneesLeft",
+                        "A_KneesRight",
+                        "A_ODST",
+                        "A_TacPad",
+                        "AS_BaseLeft",
+                        "AS_BaseRight",
+                        "AS_LargeRight",
+                        "AS_MediumLeft",
+                        "AS_ODSTCQBLeft",
+                        "AS_ODSTCQBRight",
+                        "AS_ODSTLeft",
+                        "AS_ODSTRight",
+                        "AS_ODSTSniperLeft",
+                        "AS_ODSTSniperRight",
+                        "AS_SmallLeft",
+                        "AS_SmallRight",
+                        "AP_BR",
+                        "AP_Canteen",
+                        "AP_GL",
+                        "AP_Knife",
+                        "AP_MGThigh",
+                        "AP_Rounds",
+                        "AP_SG",
+                        "AP_SMG",
+                        "AP_Sniper",
+                        "AP_Frag",
+                        "APO_AR",
+                        "APO_BR",
+                        "APO_SMG",
+                        "APO_Sniper",
+                        "CustomKit_Scorch"};
+  }
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\marine\armors\vest_alpha_co.paa",
+      "armco_armours\data\marine\armors\armour_alpha_3_co.paa",
+      "armco_armours\data\marine\armors\legs_alpha_co.paa",
+      "optre_unsc_units\army\data\ghillie_woodland_co.paa",
+      "optre_unsc_units\army\data\odst_armor_co.paa"};
+};
+class ARMCO_Alpha_EOD_Armor : ARMCO_Alpha_Armor {
+  dlc = "ARMCO";
+  scope = 2;
+  ScopeArsenal = 2;
+  author = "Belhun";
+  displayName = "[ARMCO][A] Alpha Armor EOD";
+  class ItemInfo: ItemInfo
+  {
+    hiddenSelections[] = {"camo",
+                        "camo2",
+                        "camo3",
+                        "camo4",
+                        "camo5",
+                        "A_Ghillie",
+                        "A_KneesLeft",
+                        "A_KneesRight",
+                        "A_ODST",
+                        "A_TacPad",
+                        "AS_BaseLeft",
+                        "AS_BaseRight",
+                        "AS_LargeRight",
+                        "AS_MediumLeft",
+                        "AS_ODSTCQBLeft",
+                        "AS_ODSTCQBRight",
+                        "AS_ODSTLeft",
+                        "AS_ODSTRight",
+                        "AS_ODSTSniperLeft",
+                        "AS_ODSTSniperRight",
+                        "AS_SmallLeft",
+                        "AS_SmallRight",
+                        "AP_BR",
+                        "AP_Canteen",
+                        "AP_GL",
+                        "AP_Knife",
+                        "AP_MGThigh",
+                        "AP_Rounds",
+                        "AP_SG",
+                        "AP_SMG",
+                        "AP_Sniper",
+                        "AP_Frag",
+                        "APO_AR",
+                        "APO_BR",
+                        "APO_SMG",
+                        "APO_Sniper",
+                        "CustomKit_Scorch"};
+  }
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\marine\armors\vest_alpha_co.paa",
+      "armco_armours\data\marine\armors\armour_alpha_EOD_co.paa",
+      "armco_armours\data\marine\armors\legs_alpha_co.paa",
+      "optre_unsc_units\army\data\ghillie_woodland_co.paa",
+      "optre_unsc_units\army\data\odst_armor_co.paa"};
+};
+class ARMCO_Alpha_Medical_Armor : ARMCO_Alpha_Armor {
+  dlc = "ARMCO";
+  scope = 2;
+  ScopeArsenal = 2;
+  author = "Belhun";
+  displayName = "[ARMCO][A] Alpha Armor Medical";
+  class ItemInfo: ItemInfo
+  {
+    hiddenSelections[] = {"camo",
+                        "camo2",
+                        "camo3",
+                        "camo4",
+                        "camo5",
+                        "A_Ghillie",
+                        "A_KneesLeft",
+                        "A_KneesRight",
+                        "A_ODST",
+                        "A_TacPad",
+                        "AS_BaseLeft",
+                        "AS_BaseRight",
+                        "AS_LargeRight",
+                        "AS_MediumLeft",
+                        "AS_ODSTCQBLeft",
+                        "AS_ODSTCQBRight",
+                        "AS_ODSTLeft",
+                        "AS_ODSTRight",
+                        "AS_ODSTSniperLeft",
+                        "AS_ODSTSniperRight",
+                        "AS_SmallLeft",
+                        "AS_SmallRight",
+                        "AP_BR",
+                        "AP_Canteen",
+                        "AP_GL",
+                        "AP_Knife",
+                        "AP_MGThigh",
+                        "AP_Rounds",
+                        "AP_SG",
+                        "AP_SMG",
+                        "AP_Sniper",
+                        "AP_Frag",
+                        "APO_AR",
+                        "APO_BR",
+                        "APO_SMG",
+                        "APO_Sniper",
+                        "CustomKit_Scorch"};
+  }
+  hiddenSelectionsTextures[] = {
+      "armco_armours\data\marine\armors\vest_alpha_co.paa",
+      "armco_armours\data\marine\armors\armour_alpha_Medical_co.paa",
+      "armco_armours\data\marine\armors\legs_alpha_co.paa",
+      "optre_unsc_units\army\data\ghillie_woodland_co.paa",
+      "optre_unsc_units\army\data\odst_armor_co.paa"};
+};
