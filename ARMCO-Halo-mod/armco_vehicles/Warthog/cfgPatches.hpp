0f51a6a95747ea5ae8aab630fc9463a609d069ed
Belhun <akcool151@gmail.com>
2023-08-08 18:23:52 -0700

    updated CfgPatches with new warhog

--- ARMCO-Halo-mod/armco_vehicles/Warthog/cfgPatches.hpp
+++ ARMCO-Halo-mod/armco_vehicles/Warthog/cfgPatches.hpp
@@ -2,7 +2,8 @@ class CfgPatches {
   class ARMCO_Warthogs {
     units[] = {"ARMCO_M12_LRV",     "ARMCO_M12G1_LRV",       "ARMCO_M12A1_LRV",
                "ARMCO_M12R_AA",     "ARMCO_M914_RV",         "ARMCO_M12_FAV",
-               "ARMCO_M12_FAV_APC", "ARMCO_M12_FAV_APC_MED", "ARMCO_M813_TT"};
+               "ARMCO_M12_FAV_APC", "ARMCO_M12_FAV_APC_MED", "ARMCO_M813_TT",
+               "ARMCO_M12_TD"};
     weapons[] = {};
     requiredVersion = 0.1;
     requiredAddons[] = {"OPTRE_UNSC_Units", "ace_main",
