4a77b1b54cd36550ea47e4342a6dda97c31f3af3
Belhun <akcool151@gmail.com>
2024-01-26 02:28:13 -0800

    Updated PBO builder to have newer PBO's

--- ARMCO-Halo-mod/Build_PBOs.bat
+++ ARMCO-Halo-mod/Build_PBOs.bat
@@ -9,9 +9,12 @@ xcopy /y /i /f "%~dp0..\example\@armcohalo\mod.cpp" "%~dp0..\..\Export\@armcohal
 C:
 cd C:\Program Files\PBO Manager v.1.4 beta
 
-PBOConsole.exe -pack %~dp0armco_vehicles       %~dp0..\..\Export\@armcohalo\Addons\armco_vehicles.pbo
-PBOConsole.exe -pack %~dp0armco_armours         %~dp0..\..\Export\@armcohalo\Addons\armco_armours.pbo
-PBOConsole.exe -pack %~dp0armco_items           %~dp0..\..\Export\@armcohalo\Addons\armco_items.pbo
-PBOConsole.exe -pack %~dp0armco_units           %~dp0..\..\Export\@armcohalo\Addons\armco_units.pbo
+PBOConsole.exe -pack %~dp0armco_vehicles            %~dp0..\..\Export\@armcohalo\Addons\armco_vehicles.pbo
+PBOConsole.exe -pack %~dp0armco_armours             %~dp0..\..\Export\@armcohalo\Addons\armco_armours.pbo
+PBOConsole.exe -pack %~dp0armco_items               %~dp0..\..\Export\@armcohalo\Addons\armco_items.pbo
+PBOConsole.exe -pack %~dp0armco_units               %~dp0..\..\Export\@armcohalo\Addons\armco_units.pbo
+PBOConsole.exe -pack %~dp0armco_ammo                %~dp0..\..\Export\@armcohalo\Addons\armco_ammo.pbo
+PBOConsole.exe -pack %~dp0darkmod_4thot_fact_nmr    %~dp0..\..\Export\@armcohalo\Addons\darkmod_4thot_fact_nmr.pbo
+PBOConsole.exe -pack %~dp0armco_ammo                %~dp0..\..\Export\@armcohalo\Addons\darkmod_4thot_m808ifv.pbo
 
 PAUSE
