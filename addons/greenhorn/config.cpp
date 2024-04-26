715f9dff9e04ee4a7d198beedfe3ceef81c70055
Belhun <akcool151@gmail.com>
2024-04-25 20:47:51 -0700

    Added Base Files For Green Horn and Vehicals Addons. Still needs Content of Addons added

---
+++ addons/greenhorn/config.cpp
@@ -0,0 +1,23 @@
+#include "script_component.hpp"
+class CfgPatches {
+    class 4thot_Greenhorn
+	{
+        weapons[] = {};
+        units[] =
+        {
+
+        };
+        magazines[] =
+        {
+
+        };
+        requiredAddons[] =
+        {
+
+        };
+
+        authors[] = {"Belhun", "Darknessvoid99"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
---
+++ addons/vehicles/config.cpp
@@ -0,0 +1,23 @@
+#include "script_component.hpp"
+class CfgPatches {
+    class 4thot_Vehicles
+	{
+        weapons[] = {};
+        units[] =
+        {
+
+        };
+        magazines[] =
+        {
+
+        };
+        requiredAddons[] =
+        {
+
+        };
+
+        authors[] = {"Belhun", "Darknessvoid99"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
