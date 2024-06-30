62e804cd0304068113d1827b7091951ff5d61b89
Belhun <akcool151@gmail.com>
2024-06-29 21:46:38 -0700

    Fixed Backpack Texture missing

--- addons/items/CfgCBackpacks.hpp
+++ addons/items/CfgCBackpacks.hpp
@@ -4,7 +4,7 @@ class ARMCO_Rucksack_Addy : ARMCO_Rucksack_Hard_Alpha {
 		"camo1"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Addy_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Addy_co.paa)
 	};
 };
 class ARMCO_Rucksack_Belhun : ARMCO_Rucksack_Hard_Alpha {
@@ -13,7 +13,7 @@ class ARMCO_Rucksack_Belhun : ARMCO_Rucksack_Hard_Alpha {
 		"camo1"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Belhun_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Belhun_co.paa)
 	};
 };
 class ARMCO_Rucksack_Blue : ARMCO_Rucksack_Hard_Alpha {
@@ -22,7 +22,7 @@ class ARMCO_Rucksack_Blue : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Blue_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Blue_co.paa)
 	};
 };
 class ARMCO_Rucksack_Burford : ARMCO_Rucksack_Hard_Alpha {
@@ -31,7 +31,7 @@ class ARMCO_Rucksack_Burford : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Burford_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Burford_co.paa)
 	};
 };
 class ARMCO_Rucksack_Boone : ARMCO_Rucksack_Soft_Alpha {
@@ -40,7 +40,7 @@ class ARMCO_Rucksack_Boone : ARMCO_Rucksack_Soft_Alpha {
 		"camo", "B_Medic", "B_Radio"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_soft_backpack_Boone_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_soft_backpack_Boone_co.paa)
 	};
 	tf_encryptionCode = "tf_west_radio_code";
 	tf_dialog = "rt1523g_radio_dialog";
@@ -56,7 +56,7 @@ class ARMCO_Rucksack_Cheese : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Cheese_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Cheese_co.paa)
 	};
 };
 class ARMCO_Rucksack_Ciro : ARMCO_Rucksack_Hard_Alpha {
@@ -65,7 +65,7 @@ class ARMCO_Rucksack_Ciro : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Ciro_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Ciro_co.paa)
 	};
 };
 class ARMCO_Rucksack_Darkness : ARMCO_Rucksack_Hard_Alpha {
@@ -74,7 +74,7 @@ class ARMCO_Rucksack_Darkness : ARMCO_Rucksack_Hard_Alpha {
 		"camo1"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Darkness_CO.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Darkness_CO.paa)
 	};
 };
 class ARMCO_Rucksack_Darman : ARMCO_Rucksack_Hard_Alpha {
@@ -83,7 +83,7 @@ class ARMCO_Rucksack_Darman : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Darman_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Darman_co.paa)
 	};
 };
 class ARMCO_Rucksack_Extortionist : ARMCO_Rucksack_Hard_Alpha {
@@ -92,7 +92,7 @@ class ARMCO_Rucksack_Extortionist : ARMCO_Rucksack_Hard_Alpha {
 		"camo1"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Extortionist_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Extortionist_co.paa)
 	};
 };
 class ARMCO_Rucksack_Garfield : ARMCO_Rucksack_Hard_Alpha {
@@ -101,7 +101,7 @@ class ARMCO_Rucksack_Garfield : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Garf_CO.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Garf_CO.paa)
 	};
 };
 class ARMCO_Rucksack_Merek : ARMCO_Rucksack_Hard_Alpha {
@@ -110,7 +110,7 @@ class ARMCO_Rucksack_Merek : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "AP_Heavy"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Merek_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Merek_co.paa)
 	};
 };
 class ARMCO_Rucksack_Mini : ARMCO_Rucksack_Hard_Alpha {
@@ -119,7 +119,7 @@ class ARMCO_Rucksack_Mini : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Mini_CO.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Mini_CO.paa)
 	};
 };
 class ARMCO_Rucksack_Nutters : ARMCO_Rucksack_Hard_Alpha {
@@ -128,7 +128,7 @@ class ARMCO_Rucksack_Nutters : ARMCO_Rucksack_Hard_Alpha {
 		"camo1"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Nutters_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Nutters_co.paa)
 	};
 };
 class ARMCO_Rucksack_Raziel : ARMCO_Rucksack_Hard_Alpha {
@@ -137,7 +137,7 @@ class ARMCO_Rucksack_Raziel : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Raz_CO.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Raz_CO.paa)
 	};
 };
 class ARMCO_Rucksack_Reaper : ARMCO_Rucksack_Hard_Alpha {
@@ -146,7 +146,7 @@ class ARMCO_Rucksack_Reaper : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Reaper_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Reaper_co.paa)
 	};
 };
 class ARMCO_Rucksack_Red : ARMCO_Rucksack_Hard_Alpha {
@@ -155,7 +155,7 @@ class ARMCO_Rucksack_Red : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_RedDragon_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_RedDragon_co.paa)
 	};
 };
 /* Left unit
@@ -165,7 +165,7 @@ class ARMCO_Rucksack_Red : ARMCO_Rucksack_Hard_Alpha {
 			"camo1"
 		};
 		  hiddenSelectionsTextures[] = {
-			QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Runic_co.paa)
+			QPATHTOF(data\custom_backpacks\odst_hard_backpack_Runic_co.paa)
 		};
 }; */
 class ARMCO_Rucksack_Shaw : ARMCO_Rucksack_Hard_Alpha {
@@ -174,7 +174,7 @@ class ARMCO_Rucksack_Shaw : ARMCO_Rucksack_Hard_Alpha {
 		"camo1", "biofoam"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Shaw_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Shaw_co.paa)
 	};
 };
 /* dosent have a texture for backpack
@@ -184,7 +184,7 @@ class ARMCO_Rucksack_Shaw : ARMCO_Rucksack_Hard_Alpha {
 			"camo1", "biofoam"
 		};
 		  hiddenSelectionsTextures[] = {
-			QPATHTOF(data\odst\custom_backpacks\.paa)
+			QPATHTOF(data\custom_backpacks\.paa)
 		};
 }; */
 class ARMCO_Rucksack_Wolffe : ARMCO_Rucksack_Hard_Alpha {
@@ -193,7 +193,7 @@ class ARMCO_Rucksack_Wolffe : ARMCO_Rucksack_Hard_Alpha {
 		"camo1"
 	};
 	hiddenSelectionsTextures[] = {
-		QPATHTOF(data\odst\custom_backpacks\odst_hard_backpack_Wolffe_co.paa)
+		QPATHTOF(data\custom_backpacks\odst_hard_backpack_Wolffe_co.paa)
 	};
 };
 // zUwU backpack
